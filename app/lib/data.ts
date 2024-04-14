2ff6fe843f2c3324802f9b26efee597b1eea9393
khadijehsadeghi <kh.sadeghi95@gmail.com>
2024-04-14 16:40:09 +0330

    change setTimeout revenue data

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -20,11 +20,11 @@ export async function fetchRevenue() {
     // Don't do this in production :)
 
     console.log('Fetching revenue data...');
-    await new Promise((resolve) => setTimeout(resolve, 3000));
+    await new Promise((resolve) => setTimeout(resolve, 1000));
 
     const data = await sql<Revenue>`SELECT * FROM revenue`;
 
-    console.log('Data fetch completed after 3 seconds.');
+    console.log('Data fetch completed after 1 seconds.');
 
     return data.rows;
   } catch (error) {
